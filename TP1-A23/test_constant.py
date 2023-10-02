b964cff27b2ff9cbf0a60da3498156adc3a43a4f
Plazziac Thomas <thomas.plazziac@telecom-sudparis.eu>
2023-10-02 22:34:50 +0200

    rapport and constant tests

---
+++ TP1-A23/test_constant.py
@@ -0,0 +1,51 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#x et y for algo [c | q | qS | qSR ], serie i : x_i and y_algo_i
+samples = {
+    'x_1': np.array([[50000], [100000], [200000], [400000]]),
+    'x_2': np.array([[500000], [1000000], [2000000], [4000000]]),
+    'x_3': np.array([[50000, 500000], [100000, 1000000], [200000, 2000000], [400000, 4000000]]),
+    'x_4': np.array([[50000], [100000], [200000], [400000]]),
+    'y_counting_1': np.array([[145.99], [199.29], [268.05], [383.12]]),
+    'y_counting_2': np.array([[85.43], [141.28], [279.83], [691.00]]),
+    'y_counting_3': np.array([[88.82], [186.74], [352.76], [802.74]]),
+    'y_quick_1': np.array([[81.90], [181.71], [416.41], [1088.59]]),
+    'y_quick_4': np.array([[136.22], [307.86], [674.91], [1481.40]]),
+    'y_quickSeuil_1': np.array([[66.45], [141.97], [326.44], [768.64]]),
+    'y_quickSeuil_4': np.array([[113.74], [339.12], [593.38], [1371.91]]),
+    'y_quickSeuilRandom_1': np.array([[70.93], [150.68], [369.08], [770.22]]),
+    'y_quickSeuilRandom_4': np.array([[59.21], [133.72], [297.01], [699.51]]),
+}
+
+
+def test_constant(algo: str, i: int):
+    """Display and save graph
+    Args:
+        algo(str): [ counting | quick | quickSeuil |quickSeuilRandom ]
+        i(int): number of the serie
+    """
+    
+    x = samples[f'x_{i}']
+    y = samples[f'y_{algo}_{i}']
+    
+    plt.plot(x, y, marker='o')
+    plt.xlabel('size')
+    plt.ylabel('time')
+    plt.title(f'Test constant {algo} série {i}')
+    plt.savefig(f'TP1-A23/tests/constant/{algo}_serie{i}.png')
+    plt.show()
+
+
+test_constant("counting", 1)
+test_constant("counting", 2)
+test_constant("counting", 3)
+
+test_constant("quick", 1)
+test_constant("quick", 4)
+
+test_constant("quickSeuil", 1)
+test_constant("quickSeuil", 4)
+
+test_constant("quickSeuilRandom", 1)
+test_constant("quickSeuilRandom", 4)
---
+++ TP1-A23/test_rapport.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def square(x):
+    return x**2
+
+square_table_1 = list(map(square, np.array([[50000], [100000], [200000], [400000]])))
+square_table_2 = list(map(square, np.array([[500000], [1000000], [2000000], [4000000]])))
+square_table_3 = list(map(square, np.array([[50000, 500000], [100000, 1000000], [200000, 2000000], [400000, 4000000]])))
+square_table_4 = list(map(square, np.array([[50000], [100000], [200000], [400000]])))
+
+samples = {
+    'x_1': square_table_1,
+    'x_2': square_table_2,
+    'x_3': square_table_3,
+    'x_4': square_table_4,
+    'y_counting_1': np.array([[145.99], [199.29], [268.05], [383.12]]),
+    'y_counting_2': np.array([[85.43], [141.28], [279.83], [691.00]]),
+    'y_counting_3': np.array([[88.82], [186.74], [352.76], [802.74]]),
+    'y_quick_1': np.array([[81.90], [181.71], [416.41], [1088.59]]),
+    'y_quick_4': np.array([[136.22], [307.86], [674.91], [1481.40]]),
+    'y_quickSeuil_1': np.array([[66.45], [141.97], [326.44], [768.64]]),
+    'y_quickSeuil_4': np.array([[113.74], [339.12], [593.38], [1371.91]]),
+    'y_quickSeuilRandom_1': np.array([[70.93], [150.68], [369.08], [770.22]]),
+    'y_quickSeuilRandom_4': np.array([[59.21], [133.72], [297.01], [699.51]]),
+}
+
+def test_rapport(algo: str, i: int):
+    """Display and save graph
+    Args:
+        algo(str): [ counting | quick | quickSeuil |quickSeuilRandom ]
+        i(int): number of the serie
+    """
+    
+    x = samples[f'x_{i}']
+    y = samples[f'y_{algo}_{i}']
+    
+    plt.plot(x, y, marker='o')
+    #plt.xscale('log')
+    #plt.yscale('log')
+    plt.xlabel('size')
+    plt.ylabel('time')
+    plt.title(f'Test rapport {algo} série {i}')
+    plt.savefig(f'TP1-A23/tests/rapport/{algo}_serie{i}.png')
+    plt.show()
+    
+test_rapport("counting", 1)
+test_rapport("counting", 2)
+test_rapport("counting", 3)
+
+test_rapport("quick", 1)
+test_rapport("quick", 4)
+
+test_rapport("quickSeuil", 1)
+test_rapport("quickSeuil", 4)
+
+test_rapport("quickSeuilRandom", 1)
+test_rapport("quickSeuilRandom", 4)
